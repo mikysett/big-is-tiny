1ad4940c1f8b27a459a16b27cbb70a538eec8ae2
Michele Sessa <mikysett@gmail.com>
2024-07-13 19:59:14 +0100

    add setup config with mocked return

---
+++ src/setup_config.go
@@ -0,0 +1,40 @@
+package main
+
+func setupConfig(configPath string) (*BigChange, error) {
+	// TODO: Replace hardcoded values with fetched ones
+	return &BigChange{
+		Settings: Settings{
+			IsDryRun:   true,
+			IsDraftPrs: false,
+			Verbose:    true,
+			RepoPath:   "../test_repo",
+			Platform:   Azure,
+		},
+		Domains: []Domain{
+			{
+				Name: "dom1",
+				Path: "domains/dom1",
+				Teams: []Team{
+					{
+						TeamUrl:  "team_url1",
+						TeamType: Communication(Slack),
+					},
+				},
+			},
+			{
+				Name: "dom2",
+				Path: "domains/dom2",
+				Teams: []Team{
+					{
+						TeamUrl:  "team_url2",
+						TeamType: Communication(Teams),
+					},
+					{
+						TeamUrl:  "team_url2bis",
+						TeamType: Communication(Slack),
+					},
+				},
+			},
+		},
+	}, nil
+}
