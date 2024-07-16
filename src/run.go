cf3ed3857c8bc23c07e02733e8fa172439443830
Michele Sessa <mikysett@gmail.com>
2024-07-16 01:01:14 +0100

    add comments to guide development and improve logs

--- src/run.go
+++ src/run.go
@@ -11,6 +11,14 @@ func run(configPath string, log *slog.Logger) error {
 	if err != nil {
 		return fmt.Errorf("failed to setup config from %s: %w", configPath, err)
 	}
-	log.Info("after configuration file", "bigChange", bigChange)
+	log.Info("config extracted from config file", "bigChange", bigChange)
+
+	// Check if the repo exists
+
+	// Checkout to main
+
+	// Checkout the files from the big change branch
+
+	// Create a single branch and then a PR for each domain
 	return nil
 }
