e9e77e8b41d91c66cff4132af5d611991eb5a6fa
Michele Sessa <mikysett@gmail.com>
2024-07-17 23:47:19 +0100

    update mocked setup config and add ctx

--- src/setup_config.go
+++ src/setup_config.go
@@ -1,39 +1,54 @@
 package main
 
-func setupConfig(configPath string) (*BigChange, error) {
+import "context"
+
+func setupConfig(ctx context.Context, configPath string) (*BigChange, error) {
 	// TODO: Replace hardcoded values with fetched ones
+
+	// Log errors accordingly when values will be properly fetched
+	// log := LoggerFromContext(ctx)
+	// log.Error("failed to setup config", "configPath", configPath, "error", err)
 	return &BigChange{
-		Settings: Settings{
-			MainBranch:    "main",
-			BranchToSplit: "big-change-to-split",
-			IsDryRun:      true,
-			IsDraftPrs:    false,
-			Verbose:       true,
-			RepoPath:      "../../test_repo",
-			Platform:      Platform(Azure),
+		Settings: &Settings{
+			MainBranch:         "main",
+			BranchToSplit:      "big-change-to-split",
+			IsDryRun:           true,
+			IsDraftPrs:         false,
+			Verbose:            true,
+			RepoPath:           "../../test_repo",
+			BranchNameTemplate: "bit-{{domain_name}}-big-change-split",
+			CommitMsgTemplate:  "implement new feature for {{domain_name}} at {{team_name_1}}({{team_url_1}}) and {{team_name_2}}({{team_url_2}})",
+			PrNameTemplate:     "{{domain_id}} {{domain_name}}: Big change split",
+			PrDescription:      "This change refers to this refactor: https://example.com",
+			Platform:           Platform(Azure),
 		},
-		Domains: []Domain{
+		Domains: []*Domain{
 			{
 				Name: "dom1",
+				Id:   "AA",
 				Path: "domains/dom1",
 				Teams: []Team{
 					{
-						TeamUrl:  "team_url1",
-						TeamType: Communication(Slack),
+						Name: "First Team AA",
+						Url:  "team_url1",
+						Type: Communication(Slack),
 					},
 				},
 			},
 			{
 				Name: "dom2",
+				Id:   "BB",
 				Path: "domains/dom2",
 				Teams: []Team{
 					{
-						TeamUrl:  "team_url2",
-						TeamType: Communication(Teams),
+						Name: "Team BB 1",
+						Url:  "team_url2",
+						Type: Communication(Teams),
 					},
 					{
-						TeamUrl:  "team_url2bis",
-						TeamType: Communication(Slack),
+						Name: "Team BB 2",
+						Url:  "team_url2bis",
+						Type: Communication(Slack),
 					},
 				},
 			},
