3866e9c42a9d0adf39276bc7c27c7e6d7ba36f6f
Michele Sessa <mikysett@gmail.com>
2024-07-17 23:47:47 +0100

    implement run command wrapper

--- src/cmd_util.go
+++ src/cmd_util.go
@@ -1,1 +1,22 @@
 package main
+
+import (
+	"context"
+	"os/exec"
+	"strings"
+)
+
+func runCmd(ctx context.Context, name string, args ...string) ([]byte, error) {
+	log := LoggerFromContext(ctx)
+
+	cmd := exec.Command(name, args...)
+	output, err := cmd.CombinedOutput()
+	if err != nil {
+		log.Error("failed to run command",
+			"command", name,
+			"args", strings.Join(args, " "),
+			"output", string(output[:]),
+			"error", err)
+	}
+	return output, err
+}
