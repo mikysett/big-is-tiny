fd3732c070b7ec11a13261dd7e9c0aae0db4d431
Michele Sessa <mikysett@gmail.com>
2024-07-20 21:27:37 +0100

    move two functions to util.go for more clarity

--- src/run.go
+++ src/run.go
@@ -139,21 +139,6 @@ func (bit *BigIsTiny) createBranch(ctx context.Context, domain *Domain, settings
 	return nil
 }
 
-func generateFromTemplate(domain *Domain, template string) string {
-	replacements := []string{
-		"{{domain_name}}", domain.Name,
-		"{{domain_id}}", domain.Id,
-	}
-	for i, team := range domain.Teams {
-		replacements = append(replacements,
-			fmt.Sprintf("{{team_name_%d}}", i+1), team.Name,
-			fmt.Sprintf("{{team_url_%d}}", i+1), team.Url,
-		)
-	}
-	r := strings.NewReplacer(replacements...)
-	return r.Replace(template)
-}
-
 func createPullRequest(ctx context.Context, domain *Domain, settings *Settings) (pr *PullRequest, err error) {
 	defer func() {
 		if err != nil {
@@ -162,18 +147,4 @@ func createPullRequest(ctx context.Context, domain *Domain, settings *Settings) 
 		}
 	}()
 	return nil, fmt.Errorf("not implemented")
-}
-
-func (bit *BigIsTiny) cleanup(ctx context.Context, bigChange *BigChange) {
-	log := LoggerFromContext(ctx)
-	log.Info("remove all branches and PRs")
-
-	_ = bit.gitOps.gitCheckout(ctx, bigChange.Settings.MainBranch)
-	for _, domain := range bigChange.Domains {
-		if domain.Branch == nil {
-			continue
-		}
-		_ = bit.gitOps.gitDeleteBranch(ctx, domain.Branch.name)
-		_ = bit.gitOps.gitDeleteRemoteBranch(ctx, bigChange.Settings.Remote, domain.Branch.name)
-	}
 }
--- src/util.go
+++ src/util.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
+	"strings"
 	"syscall"
 )
 
@@ -15,6 +16,35 @@ func chdirWithLogs(ctx context.Context, path string) error {
 		return err
 	}
 	return nil
+}
+
+func generateFromTemplate(domain *Domain, template string) string {
+	replacements := []string{
+		"{{domain_name}}", domain.Name,
+		"{{domain_id}}", domain.Id,
+	}
+	for i, team := range domain.Teams {
+		replacements = append(replacements,
+			fmt.Sprintf("{{team_name_%d}}", i+1), team.Name,
+			fmt.Sprintf("{{team_url_%d}}", i+1), team.Url,
+		)
+	}
+	r := strings.NewReplacer(replacements...)
+	return r.Replace(template)
+}
+
+func (bit *BigIsTiny) cleanup(ctx context.Context, bigChange *BigChange) {
+	log := LoggerFromContext(ctx)
+	log.Info("remove all branches and PRs")
+
+	_ = bit.gitOps.gitCheckout(ctx, bigChange.Settings.MainBranch)
+	for _, domain := range bigChange.Domains {
+		if domain.Branch == nil {
+			continue
+		}
+		_ = bit.gitOps.gitDeleteBranch(ctx, domain.Branch.name)
+		_ = bit.gitOps.gitDeleteRemoteBranch(ctx, bigChange.Settings.Remote, domain.Branch.name)
+	}
 }
 
 func (e Communication) String() string {
