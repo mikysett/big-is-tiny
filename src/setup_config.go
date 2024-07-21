3f0c33717030a6f33f4759967534e5a7e556da96
Michele Sessa <mikysett@gmail.com>
2024-07-21 14:18:01 +0100

    add templated values to PR description example

--- src/setup_config.go
+++ src/setup_config.go
@@ -19,7 +19,7 @@ func setupConfig(ctx context.Context, configPath string) (*BigChange, error) {
 			BranchNameTemplate: "bit-{{domain_name}}-big-change-split",
 			CommitMsgTemplate:  "implement new feature for {{domain_name}} at {{team_name_1}}({{team_url_1}}) and {{team_name_2}}({{team_url_2}})",
 			PrNameTemplate:     "{{domain_id}} {{domain_name}}: Big change split",
-			PrDescription:      "This change refers to this refactor: https://example.com",
+			PrDescTemplate:     "This change refers to this refactor for domain {{domain_id}} {{domain_name}}: https://example.com",
 			Platform:           Platform(Azure),
 		},
 		Domains: []*Domain{
