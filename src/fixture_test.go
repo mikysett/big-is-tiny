19eee480c9db577e1297453189862ed9f362e5fa
Michele Sessa <mikysett@gmail.com>
2024-07-21 14:38:14 +0100

    update unit tests with fixture for config

--- src/fixture_test.go
+++ src/fixture_test.go
@@ -19,6 +19,58 @@ func fixtureFlags(mods ...func(*Flags)) *Flags {
 	return flags
 }
 
+func fixtureBigChange(mods ...func(*BigChange)) *BigChange {
+	flags := &BigChange{
+		Settings: &Settings{
+			MainBranch:         "main",
+			Remote:             "origin",
+			BranchToSplit:      "big-change-to-split",
+			IsDraftPrs:         false,
+			RepoPath:           "../../test_repo",
+			BranchNameTemplate: "bit-{{domain_name}}-big-change-split",
+			CommitMsgTemplate:  "implement new feature for {{domain_name}} at {{team_name_1}}({{team_url_1}}) and {{team_name_2}}({{team_url_2}})",
+			PrNameTemplate:     "{{domain_id}} {{domain_name}}: Big change split",
+			PrDescTemplate:     "This change refers to this refactor for domain {{domain_id}} {{domain_name}}: https://example.com",
+			Platform:           Platform(Azure),
+		},
+		Domains: []*Domain{
+			{
+				Name: "dom1",
+				Id:   "AA",
+				Path: "domains/dom1/",
+				Teams: []Team{
+					{
+						Name: "First Team AA",
+						Url:  "https://example_1.com",
+						Type: Communication(Slack),
+					},
+				},
+			},
+			{
+				Name: "dom2",
+				Id:   "BB",
+				Path: "domains/dom2/",
+				Teams: []Team{
+					{
+						Name: "Team BB 1",
+						Url:  "https://example_2.com",
+						Type: Communication(Teams),
+					},
+					{
+						Name: "Team BB 2",
+						Url:  "https://example_2_bis.com",
+						Type: Communication(Slack),
+					},
+				},
+			},
+		},
+	}
+	for _, mod := range mods {
+		mod(flags)
+	}
+	return flags
+}
+
 func fixtureGitOps(mods ...func(*GitOps)) *GitOps {
 	gitOps := &GitOps{
 		gitCheckout:           func(ctx context.Context, s string) error { return nil },
--- src/run_test.go
+++ src/run_test.go
@@ -10,6 +10,7 @@ type given struct {
 	chdirWithLogs func(context.Context, string) error
 	flags         *Flags
 	gitOps        *GitOps
+	config        *BigChange
 }
 
 var runTests = []struct {
@@ -23,6 +24,7 @@ var runTests = []struct {
 			chdirWithLogs: fixtureChdirWithLogs(),
 			flags:         fixtureFlags(),
 			gitOps:        fixtureGitOps(),
+			config:        fixtureBigChange(),
 		},
 	},
 	{
@@ -39,6 +41,7 @@ var runTests = []struct {
 					return fmt.Errorf("gitPushSetUpstream should not be called")
 				}
 			}),
+			config: fixtureBigChange(),
 		},
 	},
 	{
@@ -47,6 +50,7 @@ var runTests = []struct {
 			chdirWithLogs: func(ctx context.Context, s string) error { return fmt.Errorf("chdir failed") },
 			flags:         fixtureFlags(),
 			gitOps:        fixtureGitOps(),
+			config:        fixtureBigChange(),
 		},
 		expectedErr: fmt.Errorf("chdir failed"),
 	},
@@ -58,6 +62,7 @@ var runTests = []struct {
 			gitOps: fixtureGitOps(func(g *GitOps) {
 				g.gitCheckout = func(ctx context.Context, s string) error { return fmt.Errorf("gitCheckout failed") }
 			}),
+			config: fixtureBigChange(),
 		},
 		expectedErr: fmt.Errorf("gitCheckout failed"),
 	},
@@ -69,6 +74,7 @@ var runTests = []struct {
 			gitOps: fixtureGitOps(func(g *GitOps) {
 				g.gitCheckoutNewBranch = func(ctx context.Context, s string) error { return fmt.Errorf("gitCheckoutNewBranch failed") }
 			}),
+			config: fixtureBigChange(),
 		},
 		expectedErr: fmt.Errorf("gitCheckoutNewBranch failed"),
 	},
@@ -80,6 +86,7 @@ var runTests = []struct {
 			gitOps: fixtureGitOps(func(g *GitOps) {
 				g.gitStatus = func(ctx context.Context) ([]byte, error) { return nil, fmt.Errorf("gitStatus failed") }
 			}),
+			config: fixtureBigChange(),
 		},
 		expectedErr: fmt.Errorf("gitStatus failed"),
 	},
@@ -91,6 +98,7 @@ var runTests = []struct {
 			gitOps: fixtureGitOps(func(g *GitOps) {
 				g.gitAdd = func(ctx context.Context, s string) error { return fmt.Errorf("gitAdd failed") }
 			}),
+			config: fixtureBigChange(),
 		},
 		expectedErr: fmt.Errorf("gitAdd failed"),
 	},
@@ -102,6 +110,7 @@ var runTests = []struct {
 			gitOps: fixtureGitOps(func(g *GitOps) {
 				g.gitCommit = func(ctx context.Context, s string) error { return fmt.Errorf("gitCommit failed") }
 			}),
+			config: fixtureBigChange(),
 		},
 		expectedErr: fmt.Errorf("gitCommit failed"),
 	},
@@ -113,6 +122,7 @@ var runTests = []struct {
 			gitOps: fixtureGitOps(func(g *GitOps) {
 				g.gitCheckoutFiles = func(ctx context.Context, s string) error { return fmt.Errorf("gitCheckoutFiles failed") }
 			}),
+			config: fixtureBigChange(),
 		},
 		expectedErr: fmt.Errorf("gitCheckoutFiles failed"),
 	},
@@ -124,6 +134,7 @@ var runTests = []struct {
 			gitOps: fixtureGitOps(func(g *GitOps) {
 				g.gitReset = func(ctx context.Context) error { return fmt.Errorf("gitReset failed") }
 			}),
+			config: fixtureBigChange(),
 		},
 		expectedErr: fmt.Errorf("gitReset failed"),
 	},
@@ -135,6 +146,7 @@ var runTests = []struct {
 			gitOps: fixtureGitOps(func(g *GitOps) {
 				g.gitPushSetUpstream = func(ctx context.Context, s1, s2 string) error { return fmt.Errorf("gitPushSetUpstream failed") }
 			}),
+			config: fixtureBigChange(),
 		},
 		expectedErr: fmt.Errorf("gitPushSetUpstream failed"),
 	},
@@ -148,7 +160,7 @@ func TestRun(t *testing.T) {
 				flags:         *tt.given.flags,
 				gitOps:        tt.given.gitOps,
 			}
-			gotErr := bit.run(context.Background())
+			gotErr := bit.run(context.Background(), tt.given.config)
 
 			// We get an error when we don't expect it or we don't get one when we expect it
 			if tt.expectedErr != nil != (gotErr != nil) {
