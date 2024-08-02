8ec0d9d6429a9930d8eea8de5c360cdc8bef227f
Michele Sessa <michele.sessa@freiehit.com>
2024-08-02 18:59:06 +0100

    implement config json parsing and validation

--- src/setup_config.go
+++ src/setup_config.go
@@ -1,51 +1,49 @@
 package main
 
-import "context"
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+)
 
-func setupConfig(ctx context.Context, configPath string) (*BigChange, error) {
-	// TODO: Replace hardcoded values with fetched ones
+func setupConfig(ctx context.Context, rawConfig []byte) (*BigChange, error) {
+	log := LoggerFromContext(ctx)
+	bigChange := &BigChange{}
 
-	// Log errors accordingly when values will be properly fetched
-	// log := LoggerFromContext(ctx)
-	// log.Error("failed to setup config", "configPath", configPath, "error", err)
-	return &BigChange{
-		Settings: &Settings{
-			MainBranch:         "main",
-			Remote:             "origin",
-			BranchToSplit:      "big-change-to-split",
-			IsDraftPrs:         false,
-			BranchNameTemplate: "bit-{{domain_name}}-big-change-split",
-			CommitMsgTemplate:  "implement new feature for {{domain_name}} at {{team_name_1}}({{team_url_1}}) and {{team_name_2}}({{team_url_2}})",
-			PrNameTemplate:     "{{domain_id}} {{domain_name}}: Big change split",
-			PrDescTemplate:     "This change refers to this refactor for domain {{domain_id}} {{domain_name}}: https://example.com",
-		},
-		Domains: []*Domain{
-			{
-				Name: "dom1",
-				Id:   "AA",
-				Path: "domains/dom1/",
-				Teams: []Team{
-					{
-						Name: "First Team AA",
-						Url:  "https://example_1.com",
-					},
-				},
-			},
-			{
-				Name: "dom2",
-				Id:   "BB",
-				Path: "domains/dom2/",
-				Teams: []Team{
-					{
-						Name: "Team BB 1",
-						Url:  "https://example_2.com",
-					},
-					{
-						Name: "Team BB 2",
-						Url:  "https://example_2_bis.com",
-					},
-				},
-			},
-		},
-	}, nil
+	err := json.Unmarshal(rawConfig, bigChange)
+	if err != nil {
+		log.Error("failed to unmarshal config", "error", err)
+		return nil, err
+	}
+
+	if bigChange.Domains == nil {
+		log.Error("missing or empty config field", "field", "BigChange.Domains")
+		return nil, fmt.Errorf("missing or empty config field")
+	}
+	if bigChange.Settings == nil {
+		log.Error("missing or empty config field", "field", "BigChange.Settings")
+		return nil, fmt.Errorf("missing or empty config field")
+	}
+
+	if bigChange.Settings.MainBranch == "" {
+		log.Error("missing or empty config field", "field", "BigChange.Settings.MainBranch")
+		return nil, fmt.Errorf("missing or empty config field")
+	}
+	if bigChange.Settings.Remote == "" {
+		log.Error("missing or empty config field", "field", "BigChange.Settings.Remote")
+		return nil, fmt.Errorf("missing or empty config field")
+	}
+	if bigChange.Settings.BranchToSplit == "" {
+		log.Error("missing or empty config field", "field", "BigChange.Settings.BranchToSplit")
+		return nil, fmt.Errorf("missing or empty config field")
+	}
+
+	for _, domain := range bigChange.Domains {
+		if domain.Path == "" {
+			log.Error("missing or empty config field", "domain name", domain.Name, "field", "Domain.Path")
+			return nil, fmt.Errorf("missing or empty config field")
+		}
+	}
+
+	return bigChange, nil
 }
